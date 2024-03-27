7836091cc8b8ce9b8230d12259d92b3d49017dbe
BeSingularTeam <besingulardemo@gmail.com>
2024-03-27 15:23:37 +0530

    card-commit

--- word-counter/src/Components/Card.js
+++ word-counter/src/Components/Card.js
@@ -26,25 +26,29 @@ const App = () => {
   const handleAddBlock = () => {
     setShowBlocks(true); // Set showBlocks state to true when the "Add Block" button is clicked
   };
+  const handleAddPicture = () => {
+    setShowBlocks(true); // Set showBlocks state to true when the "Add Block" button is clicked
+  };
+
 
   return (
     <>
       <Divider orientation="left"></Divider>
-      {!showBlocks && ( // Only show the "Add Block" button if showBlocks is false
-        <div style={{ textAlign: 'center', marginTop: '50px' }}>
-          <Button type="primary" onClick={handleAddBlock}>Add Block</Button>
+      {!showBlocks && ( 
+        <div style={{ textAlign: 'center', marginTop: '50px', margin:'20px'}}>
+          <Button type="primary" style={{ marginRight: '10px' }} onClick={handleAddBlock}>Add Block</Button>
+          <Button type="primary" style={{ marginLeft: '10px' }}  onClick={handleAddPicture}>Picture Block</Button>
         </div>
       )}
       {showBlocks && ( // Show blocks if showBlocks is true
         <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
           <Col className="gutter-row" span={24}>
             <div className="wrapper">
-              <div className="wrapper_card">
-              <Text className='text'>WORDS</Text>
+              <Text className='text' style={{ color: '#B6BBC5' }}>WORDS</Text>
               <Text className="word-count">{`${wordCount}`}</Text>
-              <Text style={{ marginLeft: '20px' }}>CHARACTERS</Text>
+              <Text style={{ marginLeft: '20px',color: '#B6BBC5' }}>CHARACTERS</Text>
               <Text className="char-count" style={{ left: '-70px' }}>{`${charCount}`}</Text>
-              <Text style={{ marginLeft: '20px' }}>SENTENCES</Text>
+              <Text style={{ marginLeft: '20px',color: '#B6BBC5'}}>SENTENCES</Text>
               <Text className="sentence-count" style={{ left: '-70px' }}>{`${sentenceCount}`}</Text>
               <Input.TextArea
                 rows={4}
@@ -52,7 +56,6 @@ const App = () => {
                 value={text}
                 onChange={handleTextChange}
               />
-            </div>
             </div>
           </Col>
           <Col className="gutter-row" span={24}>
